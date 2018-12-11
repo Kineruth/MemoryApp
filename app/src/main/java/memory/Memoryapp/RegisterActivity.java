9b56b26892bfd48a98df61d69fcf3accaca77c9b
yuval <yuvalmiz9@gmail.com>
2018-12-11 13:00:26 +0200

    added the option to register to the system

--- app/src/main/java/memory/Memoryapp/RegisterActivity.java
+++ app/src/main/java/memory/Memoryapp/RegisterActivity.java
@@ -1,12 +1,18 @@
 package memory.Memoryapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.Email;
@@ -34,6 +40,9 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
     private EditText password;
 
     private Validator validator;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +55,8 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
         findViewById(R.id.btnRegister).setOnClickListener(this);
         validator = new Validator(this);
         validator.setValidationListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -75,11 +86,47 @@ public class RegisterActivity extends AppCompatActivity implements Validator.Val
         if (valIsDone) {
             switch (v.getId()) {
                 case (R.id.btnRegister):
+                    CreateNewAccount();
                     Intent intent = new Intent(this, LoginActivity.class);
                     startActivity(intent);
                     finish();
                     break;
             }
         }
+    }
+
+    private void CreateNewAccount(){
+        final String mail = email.getText().toString();
+        final String first = firstName.getText().toString();
+        final String last = lastName.getText().toString();
+        String pass = password.getText().toString();
+
+
+
+        mAuth.createUserWithEmailAndPassword(mail,pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(mail, first , last);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(RegisterActivity.this, task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                                    }
+                                    else{
+                                        Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
---
+++ app/src/main/java/memory/Memoryapp/User.java
@@ -0,0 +1,15 @@
+package memory.Memoryapp;
+
+public class User {
+
+    public String email, firstName , lastName;
+
+    public User() {
+    }
+
+    public User(String email, String first_name, String last_name) {
+        this.email = email;
+        this.firstName = first_name;
+        this.lastName = last_name;
+    }
+}
