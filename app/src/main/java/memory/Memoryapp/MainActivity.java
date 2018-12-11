1df4c56e93219da5a0eca6581b69cc90b71a648c
yuval <yuvalmiz9@gmail.com>
2018-12-11 11:29:06 +0200

    added the main activity and the fragments of the diaries

---
+++ app/src/main/java/memory/Memoryapp/MainActivity.java
@@ -0,0 +1,31 @@
+package memory.Memoryapp;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabAccessAdapter myTabAccessAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("MemoryApp");
+
+        myViewPager = findViewById(R.id.main_tabs_pager);
+        myTabAccessAdapter = new TabAccessAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabAccessAdapter);
+        myTabLayout = findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+}
--- app/src/main/java/memory/Memoryapp/WelcomeActivity.java
+++ app/src/main/java/memory/Memoryapp/WelcomeActivity.java
@@ -5,15 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         findViewById(R.id.agreeNContinueTVBtn).setOnClickListener(this);
+        init();
     }
 
+    private FirebaseAuth mAuth;
+    private void init()
+    {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() == null) {
+            onAuthSuccess(mAuth.getCurrentUser());
+        }
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+        finish();
+    }
 
     @Override
     public void onClick(View v) {
