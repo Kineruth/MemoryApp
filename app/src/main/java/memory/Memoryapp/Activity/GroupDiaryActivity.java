7b73186d0c24211012dc33d5b6f968c1d39fda9a
yuval <yuvalmiz9@gmail.com>
2018-12-23 21:27:01 +0200

    start the option to saerch in the diary

--- app/src/main/java/memory/Memoryapp/Activity/GroupDiaryActivity.java
+++ app/src/main/java/memory/Memoryapp/Activity/GroupDiaryActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import memory.Memoryapp.R;
 
@@ -16,10 +17,10 @@ public class GroupDiaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_diary);
-        initFireBase();
+        initFields();
     }
 
-    private void initFireBase() {
+    private void initFields() {
         mToolbar = findViewById(R.id.group_diary_toolbar);
         setSupportActionBar(mToolbar);
     }
@@ -27,7 +28,7 @@ public class GroupDiaryActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.group_menu,menu);
+        getMenuInflater().inflate(R.menu.group_diary_menu,menu);
         return true;
     }
 
@@ -37,8 +38,11 @@ public class GroupDiaryActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.add_members_option){
             addMembersActivity();
         }
-        if(item.getItemId() == R.id.add_memory_option){
+        if(item.getItemId() == R.id.add_group_memory_option){
             addMemoryActivity();
+        }
+        if(item.getItemId() == R.id.group_diary_search){
+            Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show();
         }
         return true;
     }
